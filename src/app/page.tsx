cd22c77cfeb6b678a2612624b28ecaa5c78016cc
Solo <creeper467@mail.ru>
2024-09-18 09:59:19 +0300

    linking data base to UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,20 @@
 import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
-const mockImg = [
-  "https://utfs.io/f/ee6a052e-e98d-44d4-b73e-0e91c8a60d89-xn84y1.png",
-  "https://utfs.io/f/5246f5e6-aaed-4267-8f0f-884e6699821b-4rer39.png",
-  "https://utfs.io/f/e4c69f80-07ed-46ce-bee9-bde7b72b600e-vin4lc.png",
-  "https://utfs.io/f/d07e25ec-46ff-4d75-803e-621422d7fd6e-4rrsep.png",
-];
-const mockData = mockImg.map((img, index) => ({
-  id: index + 1,
-  img,
-}));
+
 export default async function HomePage() {
-  const post = await db.query.posts.findMany();
-  console.log(post);
+  const images = await db.query.cool_images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-3">
-        {post.map((post) => (
-          <div key={post.id}>{post.name}</div>
+        {images.map((image) => (
+          <div key={image.id} className="w-1/3">
+            <img src={image.url} alt={image.name} />
+            <p>{image.name}</p>
+          </div>
         ))}
-        {mockData.map((img) => {
-          return (
-            <div key={img.id}>
-              {" "}
-              <img className="w-48" src={img.img} />{" "}
-            </div>
-          );
-        })}
       </div>
     </main>
   );
