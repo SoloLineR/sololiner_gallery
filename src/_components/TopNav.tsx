a0c171a099857d303cf1f1eaaedb26a5ae6e9520
Solo <creeper467@mail.ru>
2024-09-18 10:34:09 +0300

    add auth

---
+++ src/_components/TopNav.tsx
@@ -0,0 +1,22 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  UserButton,
+} from "@clerk/nextjs";
+export default function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>Sololinergallery</div>
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
--- src/components/TopNav.tsx
+++
@@ -1,8 +0,0 @@
-export default function TopNav() {
-  return (
-    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>Sololinergallery</div>
-      <div>Sign In</div>
-    </nav>
-  );
-}
