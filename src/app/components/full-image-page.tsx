2e4012f5f94aa80da880c4a3a59cfce3455d2921
Solo <creeper467@mail.ru>
2024-09-18 19:04:22 +0300

    fix modal

--- src/app/components/full-image-page.tsx
+++ src/app/components/full-image-page.tsx
@@ -6,5 +6,18 @@ export default async function FullPageImg({
   props: { id: number };
 }) {
   const images = await getImage(props.id);
-  return <img src={images.url} alt={images.name} className="w-96" />;
+  return (
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex flex-shrink items-center justify-center">
+        <img
+          src={images.url}
+          alt={images.name}
+          className="flex-shrink object-contain"
+        />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="text-xl font-bold">{images.name}</div>
+      </div>
+    </div>
+  );
 }
