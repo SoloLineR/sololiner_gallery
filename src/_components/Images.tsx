bdcb9036a063c1d8ce47036af23835ce1798573e
Solo <creeper467@mail.ru>
2024-09-18 17:32:53 +0300

    use next image

--- src/_components/Images.tsx
+++ src/_components/Images.tsx
@@ -1,12 +1,12 @@
 import { getMyImages } from "~/server/queries";
-
+import Image from "next/image";
 export default async function Images() {
   const images = await getMyImages();
   return (
     <div className="flex flex-wrap gap-3">
       {images.map((image) => (
-        <div key={image.id} className="w-1/3">
-          <img src={image.url} alt={image.name} />
+        <div key={image.id} className="flex size-48 flex-col">
+          <Image src={image.url} alt={image.name} width={192} height={192} />
           <p>{image.name}</p>
         </div>
       ))}
