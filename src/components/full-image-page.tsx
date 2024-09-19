12243488ffb14943a41170413925314460476fac
Solo <creeper467@mail.ru>
2024-09-19 11:14:52 +0300

    add delete button

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,5 +1,7 @@
 import { clerkClient } from "@clerk/nextjs/server";
-import { getImage } from "~/server/queries";
+import { deleteImage, getImage } from "~/server/queries";
+import { Button } from "./ui/button";
+import { redirect } from "next/navigation";
 
 export default async function FullPageImg({
   props,
@@ -7,6 +9,7 @@ export default async function FullPageImg({
   props: { id: number };
 }) {
   const images = await getImage(props.id);
+
   const uploderInfo = await clerkClient.users.getUser(images.userId);
   return (
     <div className="flex h-full w-full min-w-0">
@@ -23,6 +26,18 @@ export default async function FullPageImg({
         </div>
         <div>Uploded by {uploderInfo.fullName}</div>
         <div>Created on {images.createdAt.toLocaleDateString()}</div>
+        <div>
+          <form
+            action={async () => {
+              "use server";
+              await deleteImage(props.id);
+            }}
+          >
+            <Button type="submit" variant="destructive">
+              Delete
+            </Button>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,6 +1,10 @@
 import "server-only";
 import { db } from "./db";
 import { auth } from "@clerk/nextjs/server";
+import { cool_images } from "./db/schema";
+import { and, eq } from "drizzle-orm";
+
+import { redirect } from "next/navigation";
 
 export async function getMyImages() {
   const user = auth();
@@ -19,8 +23,20 @@ export async function getImage(id: number) {
   const image = await db.query.cool_images.findFirst({
     where: (model, { eq }) => eq(model.id, id),
   });
+
   if (!image) throw new Error("Image not found");
 
   if (image.userId !== user.userId) throw new Error("Unauthorized");
   return image;
+}
+
+export async function deleteImage(id: number) {
+  const user = auth();
+  if (!user.userId) throw new Error("Unauthorized");
+
+  await db
+    .delete(cool_images)
+    .where(and(eq(cool_images.id, id), eq(cool_images.userId, user.userId)));
+
+  redirect("/");
 }
